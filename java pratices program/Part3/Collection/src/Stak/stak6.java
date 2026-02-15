2fe9124ec589440d2d44660afe09de0e9051bfbc
muskan <muskantuteja714@gmail.com>
2026-02-15 16:33:05 +0530

    fg

--- java pratices program/Part3/Collection/src/Stak/stak6.java
+++ java pratices program/Part3/Collection/src/Stak/stak6.java
@@ -1,5 +1,20 @@
 package Stak;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class stak6 {
+	public static void main(String[] args) {
+		Stack<Boolean> a= new Stack<>();
+		System.out.println(a.isEmpty());
+		a.add(true);
+		a.add(false);
+		a.add(null);
+	    System.out.println(a);
+		System.out.println(a.isEmpty());
+		System.out.println(a.size());
+		
+
+		}
 
 }
